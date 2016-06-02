a462db3bb23ed3018081c166c9ac9b4fee65cfa1
Atie <Atiedag@gmail.com>
2016-06-02 16:34:45 +0200

    Problem 10 done

--- Problem10.py
+++ Problem10.py
@@ -1,10 +1,9 @@
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 # Find the sum of all the primes below two million.
 
-import math
 import numpy
 
-up_to = 100
+up_to = 500
 # 1 Gör en lista över alla tal från två till något valbart största tal n.
 prim_numbers = numpy.arange(2, up_to, 1)
 # 2 Stryk ut från listan alla jämna tal som är större än två (4, 6, 8 osv.)
@@ -12,18 +11,12 @@ prim_numbers = numpy.arange(2, up_to, 1)
 # 4 Stryk ut alla tal, som är både större än det primtalet du hittade i föregående steget och multiplar av det.
 # 5 Upprepa stegen 3 och 4 tills du har nått ett nummer som är större än kvadratroten av n.
 
-start_value = 2
-start_index = 2
-while start_value <= math.sqrt(up_to)+1:
-    # 5 Upprepa stegen 3 och 4 tills du har nått ett nummer som är större än kvadratroten av n
-
-    index = numpy.arange(start_value, up_to, start_index)
-    prim_numbers = numpy.delete(prim_numbers, index)
-    start_value += 2
+start_index = 0
+lates_prime = prim_numbers[start_index]
+while lates_prime < numpy.sqrt(up_to):
+    prim_numbers = prim_numbers[(prim_numbers % lates_prime != 0) | (prim_numbers <= lates_prime)]
+    lates_prime = prim_numbers[start_index]
     start_index += 1
 
-    print(prim_numbers)
-
 # 6 Alla kvarstående tal i listan är primtal.
-print(sum(prim_numbers))
-
+print(numpy.sum(prim_numbers, dtype=numpy.int64))
--- test.py
+++ test.py
@@ -1,4 +1,13 @@
-
+import numpy
 import timeit
 timer1 = timeit.timeit('list(range(0, 200000 - 1))', number=10)
 print(timer1)
+
+
+up_to = 200
+
+prim_numbers = numpy.arange(2, up_to, 1)
+
+sum_prime = 0
+for prime in numpy.ndenumerate(prim_numbers):
+    print(prime)
